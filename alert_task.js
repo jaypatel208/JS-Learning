e554eb5286ae6f6315cedc3c5c51e7c6841cf6e5
Jay Patel <jay037493@gmail.com>
2024-03-06 19:28:19 +0530

    Data types done

--- alert_task.js
+++ alert_task.js
@@ -30,6 +30,45 @@ let currentUserName = "John";
 
 // Data types
 // There are eight basic data types in JavaScript
+console.log(1 / 0);
+console.log(Infinity);
+console.log("not a number" / 2);
+
+// the "n" at the end means it's a BigInt
+const bigInt = 1234567890123456789012345678901234567890n;
+console.log(bigInt);
+
+// String
+let str = "Hello";
+let str2 = 'Single quotes are ok too';
+let phrase = `can embed another ${str}`;
+
+// Boolean (logical type)
+let nameFieldChecked = true;
+let ageFieldChecked = false;
+
+let isGreater = 4 > 1;
+console.log(`isGreater: ${isGreater}`);
+
+// The “null” value
+let fwb = null
+
+// The “undefined” value
+let undifined;
+console.log(undifined);
+
+// Objects and Symbols
+// The typeof operator
+console.log(`Type of isGreater: ${typeof isGreater}`);
+console.log(`Type of undefined: ${typeof undefined}`)
+
+// Task: What is the output of the script?
+let nameTask2 = "Ilya";
+console.log( `hello ${1}` );
+
+console.log( `hello ${"name"}` );
+
+console.log( `hello ${nameTask2}` );
 
 // Error here
 alert("Hello")
