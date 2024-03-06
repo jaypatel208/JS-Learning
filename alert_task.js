f30a49d29df8a414841c78695bbc0fc062d72bd0
Jay Patel <jay037493@gmail.com>
2024-03-06 18:51:17 +0530

    Learning data types

---
+++ alert_task.js
@@ -0,0 +1,36 @@
+alert('this alert is from exeternal JS');
+
+let message;
+message = 'I am variable';
+alert(message);
+
+// declaring multiple vaiable in single line
+let user = 'John', age = 57, lastName = 'Wick';
+
+// declaring multiple vaiable multi line
+let user1 = 'Wade'
+let age2 = 45
+let lastName2 = 'willson'
+
+// const variables can't be reassigned
+const learningDate = '06.03.2024';
+
+
+// Task: Working with variables
+let adminTask;
+let nameTask;
+
+nameTask = 'John Task';
+adminTask = nameTask;
+alert(adminTask);
+
+// Task: Giving the right name
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+
+// Data types
+// There are eight basic data types in JavaScript
+
+// Error here
+alert("Hello")
+[1, 2].forEach(alert);
